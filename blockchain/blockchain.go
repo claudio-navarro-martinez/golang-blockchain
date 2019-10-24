567f2df97af04f1cafbf59a11d342e3165b47dbe
claudio-navarro-martinez <claudio.martinez@uk.clara.net>
2019-10-24 14:11:26 +0100

    anaden modulos

---
+++ blockchain/blockchain.go
@@ -0,0 +1,10 @@
+package blockchain
+
+import {
+	"fmt"
+}
+const kkmodulo = 33
+
+func imprimekkmodulo(esto int) {
+	fmt.Println("llamada desde modulo %d", esto)
+}
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"rsc.io/quote"
+	"github.com/claudio-navarro-martinez/golang-blockchain/blockchain"
 )
 
 func main() {
 	fmt.Println(quote.Hello())
+	blockchain.imprimekkmodulo(9)
 }
